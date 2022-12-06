4d69caad83c6136641298941072bc933e67fb804
Timo Kühne <22354443+H3nkl3r@users.noreply.github.com>
2022-12-06 22:41:11 +0100

    reformatted LUT

--- PSOMAB/LUT.cpp
+++ PSOMAB/LUT.cpp
@@ -1,47 +1,42 @@
 #include "LUT.h"
 
+node *LUT::makeEmpty(node *t) {
+  if (t == NULL)
+    return NULL;
+  {
+    makeEmpty(t->left);
+    makeEmpty(t->right);
+    delete t;
+  }
+  return NULL;
+}
 
-node* LUT::makeEmpty(node* t) {
-        if(t == NULL)
-            return NULL;
-        {
-            makeEmpty(t->left);
-            makeEmpty(t->right);
-            delete t;
-        }
-        return NULL;
-    }
-    
-    
-int LUT::find(node* t, int128_t search_index) { 
-        if(t == NULL)
-            return -1;
-        else if(search_index < t->search_index)
-            return find(t->left, search_index);
-        else if(search_index > t->search_index)
-            return find(t->right, search_index);
-        else
-            return t->arm_index;
-    }    
-    
-node* LUT::insert(int arm_index, int128_t search_index, node* t)  
-    {
-        if(t == NULL)
-        {
-            t = new node;
-            t->search_index = search_index;
-            t->arm_index = arm_index;
-            t->left = t->right = NULL;
-        }
-        else if(search_index < t->search_index)
-            t->left = insert(arm_index,search_index, t->left);
-        else if(search_index > t->search_index)
-            t->right = insert(arm_index,search_index, t->right);
-        return t;
-    }
+int LUT::find(node *t, int128_t search_index) {
+  if (t == NULL)
+    return -1;
+  else if (search_index < t->search_index)
+    return find(t->left, search_index);
+  else if (search_index > t->search_index)
+    return find(t->right, search_index);
+  else
+    return t->arm_index;
+}
 
-node* LUT::clone(node * ptr){
-  if (ptr)  {
+node *LUT::insert(int arm_index, int128_t search_index, node *t) {
+  if (t == NULL) {
+    t = new node;
+    t->search_index = search_index;
+    t->arm_index = arm_index;
+    t->left = t->right = NULL;
+  } else if (search_index < t->search_index)
+    t->left = insert(arm_index, search_index, t->left);
+  else if (search_index > t->search_index)
+    t->right = insert(arm_index, search_index, t->right);
+  return t;
+}
+
+node *LUT::clone(node *ptr) {
+  if (ptr) {
     //node *newnode = new node(ptr->data);
     //newnode->left = clone(ptr->left);
     //newnode->right = clone(ptr->right);
@@ -51,28 +46,24 @@ node* LUT::clone(node * ptr){
   return NULL;
   //std::cout << "33" <<std::endl;
 }
-    
-LUT::LUT()
-{
-    root=NULL;
-    //std::cout << "create"<< std::endl;
+
+LUT::LUT() {
+  root = NULL;
+  //std::cout << "create"<< std::endl;
 }
 
 // PSO
-LUT::LUT(const LUT& old_LUT)
-{
-    //std::cout << "copy" << std::endl;
-    //*root = *old_LUT.root;
-    
-    root=clone(old_LUT.root);
+LUT::LUT(const LUT &old_LUT) {
+  //std::cout << "copy" << std::endl;
+  //*root = *old_LUT.root;
+
+  root = clone(old_LUT.root);
 }
 // PSO
 
-
-LUT::~LUT()
-{
-    root=makeEmpty(root);
-    //std::cout << "delete"<< std::endl;
+LUT::~LUT() {
+  root = makeEmpty(root);
+  //std::cout << "delete"<< std::endl;
 }
 
 /*
@@ -87,4 +78,3 @@ BST<T>::Node * BST<T>::clone(Node * ptr) const
   }
   return NULL;
 }*/
-
