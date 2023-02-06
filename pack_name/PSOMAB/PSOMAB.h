eef414c9ac3bef504cc631be1c63645e981f1463
Timo Kühne <22354443+H3nkl3r@users.noreply.github.com>
2023-02-06 20:59:51 +0100

    move solution class to own file

--- pack_name/PSOMAB/PSOMAB.h
+++ pack_name/PSOMAB/PSOMAB.h
@@ -11,6 +11,7 @@
 #include <vector>
 #include "Eigen/Core"
 #include <unordered_set>
+#include "Solution.h"
 
 struct MS_element {
         int arm_index;
@@ -22,18 +23,7 @@ struct MS_element {
         MS_element(int arm_index, double Q) : arm_index(arm_index), Q(Q) {}
 };
 
-struct solution {
-        int obs_number;    // the "obs-number"-th observation over the course of time
-        Eigen::VectorXi x;// x
-        int N;
-        double mean_func_val = 0.0;// meanfunction value
-        double true_func_val = 0.0;// truefunction value
 
-        solution(int obs_number, Eigen::VectorXi x, int N, double mean_func_val,
-                 double true_func_val)
-            : obs_number(obs_number), x(std::move(x)), N(N), mean_func_val(mean_func_val),
-              true_func_val(true_func_val) {}
-};
 
 class PSOMAB {
        private:
---
+++ pack_name/PSOMAB/Solution.h
@@ -0,0 +1,20 @@
+#ifndef PSOMAB_PACK_NAME_PSOMAB_SOLUTION_H_
+#define PSOMAB_PACK_NAME_PSOMAB_SOLUTION_H_
+
+#include "Eigen/Core"
+#include <algorithm>
+class solution {
+        public:
+        int obs_number;    // the "obs-number"-th observation over the course of time
+        Eigen::VectorXi x;// x
+        int N;
+        double mean_func_val = 0.0;// meanfunction value
+        double true_func_val = 0.0;// truefunction value
+
+        solution(int obs_number, Eigen::VectorXi x, int N, double mean_func_val,
+                 double true_func_val)
+            : obs_number(obs_number), x(std::move(x)), N(N), mean_func_val(mean_func_val),
+              true_func_val(true_func_val) {}
+};
+
+#endif//PSOMAB_PACK_NAME_PSOMAB_SOLUTION_H_
