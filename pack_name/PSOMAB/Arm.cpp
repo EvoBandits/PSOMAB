4151c0e9b661313fa8d4f49a8d6e64d2f93f4a9d
Timo Kühne <22354443+H3nkl3r@users.noreply.github.com>
2023-02-08 15:44:41 +0100

    rename variables

--- pack_name/PSOMAB/Arm.cpp
+++ pack_name/PSOMAB/Arm.cpp
@@ -1,50 +1,36 @@
 #include "Arm.h"
 #include <algorithm>
-#include <chrono>
-#include <future>
-#include <iostream>
-#include <thread>
 #include <utility>
-#include <vector>
 
-void Arm::reset_arm() {
-        r = 0.0;
-        Q = 0.0;
-        k = 0.0;
+double Arm::function_value() const {
+        return arm_func_(action_vector_);
 }
 
-double Arm::function_value() {
-        return arm_function(action_vector);
-}
-
-double Arm::pull_arm() {
+double Arm::pull() {
         double G = (*this).function_value();
 
-        r = r + G;
-        k = k + 1;
+        reward_ = reward_ + G;
+        num_pulls_ = num_pulls_ + 1;
 
         return G;
 }
 
-double Arm::get_Q() { return Q; }
+double Arm::get_Q() const { return Q_; }
 
-double Arm::get_k() { return k; }
+double Arm::num_pulls() const { return num_pulls_; }
 
-double Arm::get_r() { return r; }
+double Arm::reward() const { return reward_; }
 
-double Arm::get_mu() { return mu; }
+void Arm::update_num_pulls(int k_var) { num_pulls_ = num_pulls_ + k_var; }
 
-void Arm::update_k(int k_var) { k = k + k_var; }
-
-void Arm::update_r(double r_var) { r = r + r_var; }
+void Arm::update_reward(double r_var) { reward_ = reward_ + r_var; }
 
 void Arm::set_action_vector_element(int index, int value) {
-        action_vector[index] = value;
+        action_vector_[index] = value;
 }
 
-Eigen::VectorXi Arm::get_action_vector() { return action_vector; }
+Eigen::VectorXi Arm::get_action_vector() const { return action_vector_; }
 
 Arm::Arm(std::function<double(Eigen::VectorXi)> func, Eigen::VectorXi permutation, double init_r,
          double init_k, double init_Q)
-    : arm_function{std::move(func)}, action_vector{std::move(permutation)}, r{init_r}, k{init_k}, Q{init_Q} {}
-
+    : arm_func_{std::move(func)}, action_vector_{std::move(permutation)}, reward_{init_r}, num_pulls_{init_k}, Q_{init_Q} {}
