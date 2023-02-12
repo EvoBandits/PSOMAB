cd5c9e566951cbe06c118cd0e6260a63d3f1271d
Timo Kühne <22354443+H3nkl3r@users.noreply.github.com>
2023-02-12 18:17:09 +0100

    add set_action_vector for class Arm

--- pack_name/Arm.h
+++ pack_name/Arm.h
@@ -18,6 +18,7 @@ class Arm {
         void update_num_pulls(int k_var);
         void update_reward(double r_var);
         void set_action_vector_element(int index, int value);
+        void set_action_vector(Eigen::VectorXi action_vector);
 
        private:
         Eigen::VectorXi action_vector_;
