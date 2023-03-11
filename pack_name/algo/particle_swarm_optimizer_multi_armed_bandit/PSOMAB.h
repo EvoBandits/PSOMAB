1c0d7d6b739d4e879d63a4965b8c403be9c8ba97
jonathan <jo.laib@gmx.de>
2023-03-11 18:35:45 +0100

    Rename variable and function

--- pack_name/algo/particle_swarm_optimizer_multi_armed_bandit/PSOMAB.h
+++ pack_name/algo/particle_swarm_optimizer_multi_armed_bandit/PSOMAB.h
@@ -33,7 +33,7 @@ class PSOMAB {
 
         std::vector<Arm> global_arms;
         LUT global_lookup_tree;
-        std::multiset<MS_element, std::less<>> global_sats;
+        std::multiset<MS_element, std::less<>> global_sat;
 
         PSO pso;
 
@@ -42,7 +42,7 @@ class PSOMAB {
         void save_current_best_solution();
         void update_global_state(int arm_index_global, double old_reward, double new_reward);
         void add_to_global_memory(int128_t search_index_global, const Arm &test);
-        int select_ucb();
+        int find_best_ucb();
 
        public:
         void optimize();
