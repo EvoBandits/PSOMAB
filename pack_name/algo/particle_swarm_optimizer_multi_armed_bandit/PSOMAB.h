008cf508a77228b656111389399b0239991286fd
Timo Kühne <22354443+H3nkl3r@users.noreply.github.com>
2023-03-20 12:06:43 +0100

    use hash function instead of solution code

--- pack_name/algo/particle_swarm_optimizer_multi_armed_bandit/PSOMAB.h
+++ pack_name/algo/particle_swarm_optimizer_multi_armed_bandit/PSOMAB.h
@@ -4,7 +4,6 @@
 #include "../../objects/solution/Solution.h"
 #include "../particle_swarm_optimizer/PSO.h"
 #include "Eigen/Core"
-#include <boost/multiprecision/cpp_int.hpp>
 #include <cmath>
 #include <map>
 #include <queue>
@@ -13,6 +12,7 @@
 #include <unordered_set>
 #include <utility>
 #include <vector>
+#include "../../util/SolutionCodeCalculation.h"
 
 
 class PSOMAB {
@@ -20,17 +20,17 @@ class PSOMAB {
         // local SATs, arm_memory, LUTs
         std::vector<std::multimap<double, int>> local_sats;
         std::vector<std::vector<Arm>> local_arm_memories;
-        std::vector<std::unordered_map<boost::multiprecision::int128_t, int>> local_lookup_tables;
+        std::vector<std::unordered_map<Eigen::VectorXi, int>> local_lookup_tables;
 
         // global SAT, arm_memory, LUT
         std::multimap<double, int> global_sat;
         std::vector<Arm> global_arm_memory;
-        std::unordered_map<boost::multiprecision::int128_t, int> global_lookup_table;
+        std::unordered_map<Eigen::VectorXi, int> global_lookup_table;
 
         PSO pso;
 
         std::vector<int> retrieve_best_solutions();
-        int get_arm_index(const Arm& particle, std::unordered_map<boost::multiprecision::int128_t, int> &lookup_table);
+        int get_arm_index(const Arm& particle, std::unordered_map<Eigen::VectorXi, int> &lookup_table);
         static void delete_sat_node(int arm_index, Arm &arm, std::multimap<double, int> &sat);
         void sample_and_update(int particle_index,  int best_individual_arm_index);
         bool budget_reached();
