bb4b0d83d17843de6556fc6866ea2f1bd2af10ae
jonathan <jo.laib@gmx.de>
2023-03-22 12:47:38 +0100

    Add imports and test different problems

--- tests/main.cpp
+++ tests/main.cpp
@@ -2,19 +2,15 @@
 #include "../pack_name/algo/particle_swarm_optimizer/PSO.h"
 #include <iostream>
 
-#include "problems/inventory.h"
+#include "problems/inventory/inventory.h"
+#include "problems/tp1/tp1.h"
+#include "problems/ackley/ackley.h"
 
 int main() {
-
-        Eigen::VectorXi x_lb(2);
-        x_lb << 1, 1;
-        Eigen::VectorXi x_ub(2);
-        x_ub << 100, 100;
-
         bool pso = false;
 
         if(pso){
-                PSO pso_instance = PSO(10, 2, x_lb, x_ub, inventory, 10000);
+                PSO pso_instance = PSO(10, inventory_dim, inventory_lb, inventory_ub, inventory, 10000);
                 pso_instance.optimize();
 
                 for (auto &best_solution : pso_instance.best_solutions()) {
@@ -22,13 +18,14 @@ int main() {
                 }
         } else {
                 //ToDo: Seed setzen
-                PSOMAB psomab_instance = PSOMAB(inventory, 10000, 10, 1, x_lb, x_ub, 2, false);
+                PSOMAB psomab_instance = PSOMAB(ackley, 10000, 10, 1, ackley_lb, ackley_ub, ackley_dim, false);
                 psomab_instance.optimize();
 
                 for (auto &best_solution : psomab_instance.best_solutions()) {
                         best_solution.print();
                 }
         }
+
 
 
         return 0;
