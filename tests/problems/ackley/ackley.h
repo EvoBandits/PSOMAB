1eab450047a517dc870da342a1b8926448e74024
jonathan <jo.laib@gmx.de>
2023-03-22 12:45:30 +0100

    Add ackley test problem

---
+++ tests/problems/ackley/ackley.h
@@ -0,0 +1,15 @@
+#ifndef PSOMAB_TESTS_PROBLEMS_ACKLEY_H_
+#define PSOMAB_TESTS_PROBLEMS_ACKLEY_H_
+
+#include "Eigen/Core"
+#include "../../../pack_name/util/RandomNumber.h"
+
+extern Eigen::Vector2i ackley_lb;
+extern Eigen::Vector2i ackley_ub;
+extern int ackley_dim;
+
+double random_number();
+double get_true_objective_value_ackley(const Eigen::VectorXi& action_vector);
+double ackley(Eigen::VectorXi action_vector, int noise_level);
+
+#endif//PSOMAB_TESTS_PROBLEMS_ACKLEY_H_
