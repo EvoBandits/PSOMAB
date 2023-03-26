2da85f1192d5f8e8fa4354a81905adb704588c66
jonathan <jo.laib@gmx.de>
2023-03-26 17:29:49 +0200

    Improve switching algorithms while testing

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,4 +1,5 @@
 #include "../pack_name/algo/particle_swarm_optimizer_multi_armed_bandit/PSOMAB.h"
+#include "../pack_name/algo/particle_swarm_optimizer_average_neighborhood/PSOAN.h"
 #include "../pack_name/algo/particle_swarm_optimizer/PSO.h"
 #include <iostream>
 
@@ -7,26 +8,30 @@
 #include "problems/ackley/ackley.h"
 
 int main() {
-        bool pso = false;
+        std::string algo = "pso";
 
-        if(pso){
+        if(algo == "pso"){
                 PSO pso_instance = PSO(10, inventory_dim, inventory_lb, inventory_ub, inventory, 10000);
                 pso_instance.optimize();
 
                 for (auto &best_solution : pso_instance.best_solutions()) {
                         best_solution.print();
                 }
-        } else {
-                //ToDo: Seed setzen
-                PSOMAB psomab_instance = PSOMAB(ackley, 10000, 10, 1, ackley_lb, ackley_ub, ackley_dim, false);
+        } else if (algo == "psoan"){
+                PSOAN psoan_instance = PSOAN(10, inventory_dim, inventory_lb, inventory_ub, inventory, 10000);
+                psoan_instance.optimize();
+
+                for (auto &best_solution : psoan_instance.best_solutions()) {
+                        best_solution.print();
+                }
+        } else if (algo == "psomab"){
+                PSOMAB psomab_instance = PSOMAB(ackley, 10000, 10, ackley_lb, ackley_ub, ackley_dim, false);
                 psomab_instance.optimize();
 
                 for (auto &best_solution : psomab_instance.best_solutions()) {
                         best_solution.print();
                 }
         }
-
-
 
         return 0;
 }
