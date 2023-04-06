71a5a257e26f308d35d4dc435bf58424e7d542d2
jonathan <jo.laib@gmx.de>
2023-04-06 22:32:38 +0200

    Add cap_velocity to PSOAN

--- pack_name/algo/particle_swarm_optimizer_average_neighborhood/PSOAN.cpp
+++ pack_name/algo/particle_swarm_optimizer_average_neighborhood/PSOAN.cpp
@@ -26,6 +26,7 @@ void PSOAN::calculate_averaged_best_individual_arms(std::vector<Eigen::VectorXd>
 void PSOAN::update_positions() {
         std::vector<Eigen::VectorXd> averaged_best_individual_arms;
         calculate_averaged_best_individual_arms(averaged_best_individual_arms);
+        if (pso.cap_velocity_) pso.calculate_max_velocity();
 
         for (int particle_index = 0; particle_index < pso.num_particle_; particle_index++) {
                 Eigen::VectorXd current_position = pso.particles_[particle_index].get_action_vector().cast<double>();
@@ -40,6 +41,7 @@ void PSOAN::update_positions() {
                 Eigen::VectorXd cognitive_component = (random_uniform_double(0,1) * pso.c1) * cognitive_direction;
 
                 Eigen::VectorXd new_velocity = old_velocity + cognitive_component + social_component;
+                if (pso.cap_velocity_) pso.cap_velocity(new_velocity);
 
                 pso.velocity_[particle_index] = new_velocity;
                 Eigen::VectorXi proposed_position = pso.particles_[particle_index].get_action_vector() + pso.velocity_[particle_index].cast<int>();
@@ -55,7 +57,7 @@ void PSOAN::update_positions() {
         }
 }
 
-PSOAN::PSOAN(int num_particle, int dimension, Eigen::VectorXi x_min, Eigen::VectorXi x_max, std::function<double(Eigen::VectorXi, int)> opti_func, int max_simulation, bool use_random_location_update) : pso(num_particle, dimension, x_min, x_max, std::move(opti_func), max_simulation, use_random_location_update) {
+PSOAN::PSOAN(int num_particle, int dimension, Eigen::VectorXi x_min, Eigen::VectorXi x_max, std::function<double(Eigen::VectorXi, int)> opti_func, int max_simulation, bool use_random_location_update, bool cap_velocity) : pso(num_particle, dimension, x_min, x_max, std::move(opti_func), max_simulation, use_random_location_update, cap_velocity) {
 }
 
 void PSOAN::optimize() {
