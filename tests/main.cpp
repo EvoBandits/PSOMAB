4db9559ca37d246546599608a451dfb59b6a11af
Timo Kühne <22354443+H3nkl3r@users.noreply.github.com>
2023-04-11 15:43:48 +0200

    add top n resampling algorithm (PSO-ERN)

--- tests/main.cpp
+++ tests/main.cpp
@@ -6,6 +6,7 @@
 #include "../pack_name/algo/particle_swarm_optimizer_learning_automaton/LAPSO.h"
 #include "../pack_name/algo/particle_swarm_optimizer_learning_automaton/PSOLA.h"
 #include "../pack_name/algo/particle_swarm_optimizer/PSO.h"
+#include "../pack_name/algo/particle_swarm_optimizer_top_n_resampling/PSOERN.h"
 #include <iostream>
 
 #include "problems/inventory/inventory.h"
@@ -13,7 +14,7 @@
 #include "problems/ackley/ackley.h"
 
 int main() {
-        std::string algo = "psola";
+        std::string algo = "psoern";
         int max_simulation = 10000;
         int num_particle = 50;
         bool use_random_location_update = false;
@@ -84,6 +85,15 @@ int main() {
                 psomab_instance.optimize();
 
                 for (auto &best_solution : psomab_instance.best_solutions()) {
+                        best_solution.print();
+                }
+        }
+        else if (algo == "psoern"){
+                std::cout << "PSOERN:" << std::endl;
+                PSOERN psoern_instance = PSOERN(num_particle, dimension, lb, ub, opti_func, max_simulation, use_random_location_update, cap_velocity);
+                psoern_instance.optimize();
+
+                for (auto &best_solution : psoern_instance.best_solutions()) {
                         best_solution.print();
                 }
         }
