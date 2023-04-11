9adcca643b00eba59e6d7ecb13e9bca522f4e913
Timo Kühne <22354443+H3nkl3r@users.noreply.github.com>
2023-04-11 14:26:23 +0200

    add equal resampling algorithm (PSO-ER)

---
+++ pack_name/algo/particle_swarm_optimizer_equal_resampling/PSOER.h
@@ -0,0 +1,18 @@
+#ifndef PSOMAB_PACK_NAME_ALGO_PARTICLE_SWARM_OPTIMIZER_EQUAL_RESAMPLING_PSOER_H_
+#define PSOMAB_PACK_NAME_ALGO_PARTICLE_SWARM_OPTIMIZER_EQUAL_RESAMPLING_PSOER_H_
+
+#include "../particle_swarm_optimizer/PSO.h"
+#include "../../util/RandomNumber.h"
+
+class PSOER {
+       private:
+        PSO pso;
+        void sample_and_update(int particle_index);
+        int num_pulls_;
+       public:
+        PSOER(int num_particle, int dimension, Eigen::VectorXi x_min, Eigen::VectorXi x_max, std::function<double(Eigen::VectorXi, int)> opti_func, int max_simulation, int num_sample, bool use_random_location_update=false, bool cap_velocity=true);
+        void optimize();
+        std::vector<solution> best_solutions();
+};
+
+#endif//PSOMAB_PACK_NAME_ALGO_PARTICLE_SWARM_OPTIMIZER_EQUAL_RESAMPLING_PSOER_H_
