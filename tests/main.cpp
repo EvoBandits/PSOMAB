88b2eae179bca530ffde14c7683818b59d906366
Jonathan Laib <97909228+jonathanlaib@users.noreply.github.com>
2023-04-18 15:07:11 +0200

    Implement functions for easy testing

--- tests/main.cpp
+++ tests/main.cpp
@@ -18,20 +18,33 @@
 #include "problems/tp1/tp1.h"
 #include "problems/tp2/tp2.h"
 
-int main() {
-        std::string time = std::to_string(std::time(nullptr));
+void single_run(const std::string &problem, const std::string &algo, int max_simulation, int num_particle, bool use_random_location_update, bool cap_velocity);
+void multiple_runs(const std::string &problem, const std::string &algo, int max_simulation, int num_particle, bool use_random_location_update, bool cap_velocity, int num_runs);
 
-        std::string algo = "psomab";
+int main() {
         int max_simulation = 10000;
         int num_particle = 50;
         bool use_random_location_update = false;
         bool cap_velocity = true;
 
+        std::string problem = "tp1";
+        std::string algo = "pso";
+
+        //single_run(problem, algo, max_simulation, num_particle, use_random_location_update, cap_velocity);
+
+        int num_runs = 100;
+        multiple_runs(problem, algo, max_simulation, num_particle, use_random_location_update, cap_velocity, num_runs);
+
+        return 0;
+}
+
+void single_run(const std::string &problem, const std::string &algo, int max_simulation, int num_particle, bool use_random_location_update, bool cap_velocity) {
+        std::string time = std::to_string(std::time(nullptr));
+
         int dimension;
         Eigen::VectorXi lb;
         Eigen::VectorXi ub;
         std::function<double(Eigen::VectorXi, int)> opti_func;
-        std::string problem = "inventory";
         if (problem == "inventory") {
                 dimension = inventory_dim;
                 lb = inventory_lb;
@@ -54,8 +67,8 @@ int main() {
                 opti_func = ackley;
         } else if (problem == "styblinski-tang") {
                 dimension = styblinski_tang_dim;
-                styblinski_tang_lb;
-                styblinski_tang_ub;
+                lb = styblinski_tang_lb;
+                ub = styblinski_tang_ub;
                 opti_func = styblinski_tang;
         }
 
@@ -154,22 +167,126 @@ int main() {
                 }
                 psoer_instance.memory_to_csv(algo + "_memory_" + time + ".csv");
         }
+}
 
-        /*
+void multiple_runs(const std::string &problem, const std::string &algo, int max_simulation, int num_particle, bool use_random_location_update, bool cap_velocity, int num_runs) {
         double mean_reward = 0;
-        int runs = 500;
+
+        int dimension;
+        Eigen::VectorXi lb;
+        Eigen::VectorXi ub;
+        std::function<double(Eigen::VectorXi, int)> opti_func;
+        if (problem == "inventory") {
+                dimension = inventory_dim;
+                lb = inventory_lb;
+                ub = inventory_ub;
+                opti_func = inventory;
+        } else if (problem == "tp1") {
+                dimension = tp1_dim;
+                lb = tp1_lb;
+                ub = tp1_ub;
+                opti_func = tp1;
+        } else if (problem == "tp2") {
+                dimension = tp2_dim;
+                lb = tp2_lb;
+                ub = tp2_ub;
+                opti_func = tp2;
+        } else if (problem == "ackley") {
+                dimension = ackley_dim;
+                lb = ackley_lb;
+                ub = ackley_ub;
+                opti_func = ackley;
+        } else if (problem == "styblinski-tang") {
+                dimension = styblinski_tang_dim;
+                lb = styblinski_tang_lb;
+                ub = styblinski_tang_ub;
+                opti_func = styblinski_tang;
+        }
 
         std::cout << std::fixed;
         std::cout << std::setprecision(3);
 
-        for (int i = 0; i < runs; i++) {
-                PSO pso_instance = PSO(num_particle, dimension, lb, ub, opti_func, max_simulation, use_random_location_update, cap_velocity);
-                pso_instance.optimize();
-                std::cout << "Run: " << i << " | best reward: " << pso_instance.best_solutions().back().true_func_val << std::endl;
-                mean_reward += pso_instance.best_solutions().back().true_func_val;
+        if (algo == "pso") {
+                std::cout << "PSO" << std::endl;
+                for (int i = 0; i < num_runs; i++) {
+                        PSO pso_instance = PSO(num_particle, dimension, lb, ub, opti_func, max_simulation, use_random_location_update, cap_velocity);
+                        pso_instance.optimize();
+                        std::cout << "Run: " << i << " | best reward: " << pso_instance.best_solutions().back().true_func_val << std::endl;
+                        mean_reward += pso_instance.best_solutions().back().true_func_val;
+                }
+        } else if (algo == "psoan") {
+                std::cout << "PSOAN" << std::endl;
+                for (int i = 0; i < num_runs; i++) {
+                        PSOAN psoan_instance = PSOAN(num_particle, dimension, lb, ub, opti_func, max_simulation, use_random_location_update, cap_velocity);
+                        psoan_instance.optimize();
+                        std::cout << "Run: " << i << " | best reward: " << psoan_instance.best_solutions().back().true_func_val << std::endl;
+                        mean_reward += psoan_instance.best_solutions().back().true_func_val;
+                }
+        } else if (algo == "psogd") {
+                std::cout << "PSOGD" << std::endl;
+                for (int i = 0; i < num_runs; i++) {
+                        PSOGD psogd_instance = PSOGD(num_particle, dimension, lb, ub, opti_func, max_simulation, use_random_location_update, cap_velocity);
+                        psogd_instance.optimize();
+                        std::cout << "Run: " << i << " | best reward: " << psogd_instance.best_solutions().back().true_func_val << std::endl;
+                        mean_reward += psogd_instance.best_solutions().back().true_func_val;
+                }
+        } else if (algo == "psoocba") {
+                std::cout << "PSOOCBA" << std::endl;
+                for (int i = 0; i < num_runs; i++) {
+                        PSOOCBA psoocba_instance = PSOOCBA(num_particle, dimension, lb, ub, opti_func, max_simulation, use_random_location_update, cap_velocity);
+                        psoocba_instance.optimize();
+                        std::cout << "Run: " << i << " | best reward: " << psoocba_instance.best_solutions().back().true_func_val << std::endl;
+                        mean_reward += psoocba_instance.best_solutions().back().true_func_val;
+                }
+        } else if (algo == "psoocbaa") {
+                std::cout << "PSOOCBAA" << std::endl;
+                for (int i = 0; i < num_runs; i++) {
+                        PSOOCBAA psoocbaa_instance = PSOOCBAA(num_particle, dimension, lb, ub, opti_func, max_simulation, use_random_location_update, cap_velocity);
+                        psoocbaa_instance.optimize();
+                        std::cout << "Run: " << i << " | best reward: " << psoocbaa_instance.best_solutions().back().true_func_val << std::endl;
+                        mean_reward += psoocbaa_instance.best_solutions().back().true_func_val;
+                }
+        } else if (algo == "psola") {
+                std::cout << "PSOLA" << std::endl;
+                for (int i = 0; i < num_runs; i++) {
+                        PSOLA psola_instance = PSOLA(num_particle, dimension, lb, ub, opti_func, max_simulation, use_random_location_update, cap_velocity);
+                        psola_instance.optimize();
+                        std::cout << "Run: " << i << " | best reward: " << psola_instance.best_solutions().back().true_func_val << std::endl;
+                        mean_reward += psola_instance.best_solutions().back().true_func_val;
+                }
+        } else if (algo == "lapso") {
+                std::cout << "LAPSO" << std::endl;
+                for (int i = 0; i < num_runs; i++) {
+                        LAPSO lapso_instance = LAPSO(num_particle, dimension, lb, ub, opti_func, max_simulation, use_random_location_update, cap_velocity);
+                        lapso_instance.optimize();
+                        std::cout << "Run: " << i << " | best reward: " << lapso_instance.best_solutions().back().true_func_val << std::endl;
+                        mean_reward += lapso_instance.best_solutions().back().true_func_val;
+                }
+        } else if (algo == "psomab") {
+                std::cout << "PSOMAB:" << std::endl;
+                for (int i = 0; i < num_runs; i++) {
+                        PSOMAB psomab_instance = PSOMAB(opti_func, max_simulation, num_particle, lb, ub, dimension, use_random_location_update, cap_velocity);
+                        psomab_instance.optimize();
+                        std::cout << "Run: " << i << " | best reward: " << psomab_instance.best_solutions().back().true_func_val << std::endl;
+                        mean_reward += psomab_instance.best_solutions().back().true_func_val;
+                }
+        } else if (algo == "psoern") {
+                std::cout << "PSOERN:" << std::endl;
+                for (int i = 0; i < num_runs; i++) {
+                        PSOERN psoern_instance = PSOERN(num_particle, dimension, lb, ub, opti_func, max_simulation, use_random_location_update, cap_velocity);
+                        psoern_instance.optimize();
+                        std::cout << "Run: " << i << " | best reward: " << psoern_instance.best_solutions().back().true_func_val << std::endl;
+                        mean_reward += psoern_instance.best_solutions().back().true_func_val;
+                }
+        } else if (algo == "psoer") {
+                std::cout << "PSOER:" << std::endl;
+                for (int i = 0; i < num_runs; i++) {
+                        PSOER psoer_instance = PSOER(num_particle, dimension, lb, ub, opti_func, max_simulation, 20, use_random_location_update, cap_velocity);
+                        psoer_instance.optimize();
+                        std::cout << "Run: " << i << " | best reward: " << psoer_instance.best_solutions().back().true_func_val << std::endl;
+                        mean_reward += psoer_instance.best_solutions().back().true_func_val;
+                }
         }
 
-        std::cout << "Mean reward: " << mean_reward / runs << std::endl;*/
-
-        return 0;
+        std::cout << "Mean reward: " << mean_reward / num_runs << std::endl;
 }
